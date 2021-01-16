3aad3efd22b268d5338648bfbe2bd1cbada4407b
Matt Hurst <matt.adam.hurst@gmail.com>
2021-01-16 16:08:45 +0000

    feat(index.js server): add index.js functionality, add node_modules to .gitignore

---
+++ server/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const app = express();
+const router = require("./router");
+
+const port = process.env.PORT || 3001;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
+module.exports = store;
