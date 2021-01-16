fab44441414e80f843dd577368c08bde2c8a60f6
Matt Hurst <matt.adam.hurst@gmail.com>
2021-01-16 16:52:05 +0000

    feat(apiservice client): add ApiService file  and userLogin and getUserPolicy functions

---
+++ client/src/apiService/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const URL = "http://localhost:3001/";
+
+export const userLogin = async (username, password) => {
+  const { data } = await axios.post(`${URL}login`, {
+    username,
+    password,
+  });
+  return data;
+};
+
+export const getUserPolicy = async () => {
+  const { data } = await axios(`${URL}`);
+  return data;
+};
