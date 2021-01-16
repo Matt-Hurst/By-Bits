c4ae40c9eeb55dfcc9db3da8b9f5b96c0954cddd
Matt Hurst <matt.adam.hurst@gmail.com>
2021-01-16 19:37:42 +0000

    test(login test file): add Login tests, create mock file and mock userPolicy

---
+++ client/src/pages/Policy/Policy.test.jsx
@@ -0,0 +1,12 @@
+import Policy from './Policy'
+import { render, screen } from '@testing-library/react'
+import { mockUserPolicy } from '../../mocks'
+
+describe('Policy Page', () => {
+
+  it('Should render policy details', () => {
+    render(<Policy userPolicy={mockUserPolicy}/>)
+    expect(screen.getByText('My Policy')).toBeInTheDocument();
+    expect(screen.getByText(/Tesla/)).toBeInTheDocument();
+  })
+})
