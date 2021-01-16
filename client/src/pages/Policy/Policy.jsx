4cddad7f6eca1e211dbc6be68fc57b513ba83984
Matt Hurst <matt.adam.hurst@gmail.com>
2021-01-16 16:58:25 +0000

    feat(policy folder): create policy page folder, add functionality to Policy.jsx

---
+++ client/src/pages/Policy/Policy.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+const Policy = ({userPolicy}) => {
+
+  const { policy, vehicle } = userPolicy
+
+  const capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  return (
+    <div>
+      <h1>
+        My Policy
+      </h1>
+      <h3>Policy reference</h3>
+      <p>{policy.policy_ref}</p>
+      <h3>Cover type</h3>
+      <p>{policy.cover}</p>
+      <h3>Car</h3>
+      <p>{capitalize(vehicle.make)} {capitalize(vehicle.model)} {capitalize(vehicle.colour)} -{vehicle.reg}</p>
+      <h3>Address</h3>
+      <p>{`${policy.address.line_1}, ${policy.address.line_2}, ${policy.address.postcode}`}</p>
+    </div>
+  )
+}
+
+export default Policy
---
+++ client/src/pages/Policy/index.js
@@ -0,0 +1,3 @@
+import PolicyPage from "./Policy";
+
+export { PolicyPage };
